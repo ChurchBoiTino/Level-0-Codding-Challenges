e08c6052411c2e6fccd28e023e87eca52a7c8d63
ChurchBoiTino <54004757+ChurchBoiTino@users.noreply.github.com>
2021-09-20 16:44:46 +0200

    Add files via upload

--- Task 0.8.js
+++ Task 0.8.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function numToTime(number)
 {
     var hours = Math.floor(number / 60);
@@ -28,3 +29,35 @@ function numToTime(number)
 
 console.log(numToTime(240));
 
+=======
+function numToTime(number)
+{
+    var hours = Math.floor(number / 60);
+    var minutes = number % 60;
+
+    if(hours > 1)
+    {
+        hours = hours + ' hours';
+    }
+    else
+    {
+        hours = hours + ' hour';
+    }
+
+    if(minutes === 0 || minutes > 1)
+    {
+        minutes = minutes + ' minutes';
+    }
+    else
+    {
+        minutes = minutes + ' minute';
+    }
+
+    var time = hours + ' and ' + minutes;
+
+    return time;
+}
+
+console.log(numToTime(240));
+
+>>>>>>> c742eaa26decd9ac4d9f542f9abaf4559cf7e808
--- Task 0.9.js
+++ Task 0.9.js
@@ -1,3 +1,26 @@
+<<<<<<< HEAD
+function returnAllVowels(randomString)
+{
+    var onlyVowels = '';
+    let vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
+
+    for(let i of randomString.toLowerCase())
+    {
+        if(vowels.includes(i))
+        {
+            onlyVowels += i + ', ';
+        }
+    }
+
+    onlyVowels = Array.from(new Set(onlyVowels.split(', '))).toString();
+
+    console.log(onlyVowels);
+
+
+}
+
+returnAllVowels('The chUrch is on Thursday');
+=======
 function returnAllVowels(randomString)
 {
     var onlyVowels = '';
@@ -19,3 +42,4 @@ function returnAllVowels(randomString)
 }
 
 console.log(returnAllVowels('The chUrch is on Thursday'));
+>>>>>>> c742eaa26decd9ac4d9f542f9abaf4559cf7e808
