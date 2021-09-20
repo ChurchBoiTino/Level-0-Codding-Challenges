c742eaa26decd9ac4d9f542f9abaf4559cf7e808
ChurchBoiTino <54004757+ChurchBoiTino@users.noreply.github.com>
2021-09-20 16:11:39 +0200

    Add files via upload

--- Task 0.9.js
+++ Task 0.9.js
@@ -3,13 +3,15 @@ function returnAllVowels(randomString)
     var onlyVowels = '';
     let vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
 
-    for(let i of randomString)
+    for(let i of randomString.toLowerCase())
     {
         if(vowels.includes(i))
         {
             onlyVowels += i + ', ';
         }
     }
+
+    onlyVowels = Array.from(new Set(onlyVowels.split(', '))).toString();
 
     return onlyVowels;
 
